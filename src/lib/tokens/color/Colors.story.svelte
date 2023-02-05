63354bea262da10f726eb2926b4d59f6959b1356
@martin.faeroey <martin.faeroy@gmail.com>
2023-02-05 15:39:50 +0100

    initial commit 2

---
+++ src/lib/tokens/color/Colors.story.svelte
@@ -0,0 +1,128 @@
+<script lang="ts">
+    import type { Hst } from '@histoire/plugin-svelte'
+    export let Hst: Hst
+</script>
+
+<Hst.Story
+    title="Design tokens/Colors"
+    layout={{ type: 'grid' }}
+>
+    <Hst.Variant title="Grey">
+        <section>
+            <div class="grey-0"></div>
+            <div class="grey-1"></div>
+            <div class="grey-2"></div>
+            <div class="grey-3"></div>
+            <div class="grey-4"></div>
+            <div class="grey-5"></div>
+            <div class="grey-6"></div>
+            <div class="grey-7"></div>
+            <div class="grey-8"></div>
+            <div class="grey-9"></div>
+            <div class="grey-99"></div>
+        </section>
+    </Hst.Variant>
+    <Hst.Variant title="Blue">
+        <section>
+            <div class="blue-0"></div>
+            <div class="blue-1"></div>
+            <div class="blue-2"></div>
+            <div class="blue-3"></div>
+            <div class="blue-4"></div>
+            <div class="blue-5"></div>
+            <div class="blue-6"></div>
+            <div class="blue-7"></div>
+            <div class="blue-8"></div>
+            <div class="blue-9"></div>
+            <div class="blue-99"></div>
+        </section>
+    </Hst.Variant>
+    <Hst.Variant title="Green">
+        <section>
+            <div class="green-0"></div>
+            <div class="green-1"></div>
+            <div class="green-2"></div>
+            <div class="green-3"></div>
+            <div class="green-4"></div>
+            <div class="green-5"></div>
+            <div class="green-6"></div>
+            <div class="green-7"></div>
+            <div class="green-8"></div>
+            <div class="green-9"></div>
+            <div class="green-99"></div>
+        </section>
+    </Hst.Variant>
+    <Hst.Variant title="Red">
+        <section>
+            <div class="red-0"></div>
+            <div class="red-1"></div>
+            <div class="red-2"></div>
+            <div class="red-3"></div>
+            <div class="red-4"></div>
+            <div class="red-5"></div>
+            <div class="red-6"></div>
+            <div class="red-7"></div>
+            <div class="red-8"></div>
+            <div class="red-9"></div>
+            <div class="red-99"></div>
+        </section>
+    </Hst.Variant>
+</Hst.Story>
+
+<style>
+    @import 'Colors.css';
+    section {
+        display: grid;
+        gap: 1rem;
+    }
+    div {
+        height: 3rem;
+    }
+    .grey-0 { background-color: var(--grey-0); }
+    .grey-1 { background-color: var(--grey-1); }
+    .grey-2 { background-color: var(--grey-2); }
+    .grey-3 { background-color: var(--grey-3); }
+    .grey-4 { background-color: var(--grey-4); }
+    .grey-5 { background-color: var(--grey-5); }
+    .grey-6 { background-color: var(--grey-6); }
+    .grey-7 { background-color: var(--grey-7); }
+    .grey-8 { background-color: var(--grey-8); }
+    .grey-9 { background-color: var(--grey-9); }
+    .grey-99 { background-color: var(--grey-99); }
+
+    .blue-0 { background-color: var(--blue-0); }
+    .blue-1 { background-color: var(--blue-1); }
+    .blue-2 { background-color: var(--blue-2); }
+    .blue-3 { background-color: var(--blue-3); }
+    .blue-4 { background-color: var(--blue-4); }
+    .blue-5 { background-color: var(--blue-5); }
+    .blue-6 { background-color: var(--blue-6); }
+    .blue-7 { background-color: var(--blue-7); }
+    .blue-8 { background-color: var(--blue-8); }
+    .blue-9 { background-color: var(--blue-9); }
+    .blue-99 { background-color: var(--blue-99); }
+
+    .green-0 { background-color: var(--green-0); }
+    .green-1 { background-color: var(--green-1); }
+    .green-2 { background-color: var(--green-2); }
+    .green-3 { background-color: var(--green-3); }
+    .green-4 { background-color: var(--green-4); }
+    .green-5 { background-color: var(--green-5); }
+    .green-6 { background-color: var(--green-6); }
+    .green-7 { background-color: var(--green-7); }
+    .green-8 { background-color: var(--green-8); }
+    .green-9 { background-color: var(--green-9); }
+    .green-99 { background-color: var(--green-99); }
+
+    .red-0 { background-color: var(--red-0); }
+    .red-1 { background-color: var(--red-1); }
+    .red-2 { background-color: var(--red-2); }
+    .red-3 { background-color: var(--red-3); }
+    .red-4 { background-color: var(--red-4); }
+    .red-5 { background-color: var(--red-5); }
+    .red-6 { background-color: var(--red-6); }
+    .red-7 { background-color: var(--red-7); }
+    .red-8 { background-color: var(--red-8); }
+    .red-9 { background-color: var(--red-9); }
+    .red-99 { background-color: var(--red-99); }
+</style>
--- svelte.config.js
+++ svelte.config.js
@@ -4,4 +4,12 @@ export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
   preprocess: vitePreprocess(),
+  package: {
+    source: 'src/lib',
+    files: (filepath) => (
+      filepath.endsWith('.story.svelte') || filepath.endsWith('.story.md')
+        ? false
+        : true
+    )
+  }
 }
