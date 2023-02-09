2bb0d0a53df64045136b3504e7bf0640c62766b9
@martin.faeroey <martin.faeroy@gmail.com>
2023-02-09 02:33:04 +0100

    Removed postcss transpiling. added lightningcss.

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import lightningcss from 'vite-plugin-lightningcss'
 import Icons from 'unplugin-icons/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
 		svelte(),
-		Icons({
-			compiler: 'svelte',
-		}),
+		Icons({ compiler: 'svelte' }),
+		lightningcss({ browserslist: ['last 2 versions', '> 0.2% in alt-EU', 'not dead'] }),
 	],
 })
