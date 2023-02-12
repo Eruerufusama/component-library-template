8df67f54596aae3139b3fb4879709f187c1bbdd6
@martin.faeroey <martin.faeroy@gmail.com>
2023-02-12 21:42:57 +0100

    Alias to lib

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 import lightningcss from 'vite-plugin-lightningcss'
 import Icons from 'unplugin-icons/vite'
 
+const path = require('path')
+
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
@@ -10,4 +12,9 @@ export default defineConfig({
 		Icons({ compiler: 'svelte' }),
 		lightningcss({ browserslist: ['last 2 versions', '> 0.2% in alt-EU', 'not dead'] }),
 	],
+	resolve: {
+		alias: {
+			'@lib': path.resolve(__dirname, './src/lib'),
+		}
+	}
 })
