6e115202d69094e4deb6235098d496cbac35ce66
@martin.faeroey <martin.faeroy@gmail.com>
2023-02-19 20:48:05 +0100

    chore: remove unused/ci-only packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,12 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import lightningcss from 'vite-plugin-lightningcss'
-import Icons from 'unplugin-icons/vite'
 import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
 		svelte(),
-		Icons({ compiler: 'svelte' }),
 		lightningcss({ browserslist: ['last 2 versions', '> 0.2% in alt-EU', 'not dead'] }),
 
 	],
